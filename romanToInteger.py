73ca701638ab0c86febc0320fe4c6a2765363fea
Farhan Hasan <hasanfarhan33@gmail.com>
2022-04-21 12:46:44 +0530

    romanToInteger

--- romanToInteger.py
+++ romanToInteger.py
@@ -1,20 +1,20 @@
-str = "LVIII"
+s = "LVIII"
 
-def romanToInteger(str):
-    if len(str) == 0:
+def romanToInteger(s):
+    if len(s) == 0:
         return 0
     else:
-        romanList = list(str)
+        romanList = list(s)
         numberToReturn = 0
         numberList = createNumberList(romanList)
         if len(numberList) == 1:
             return numberList[0]
 
-        for i in range(len(numberList) - 1):
-            if numberList[i] >= numberList[i+1]:
-                numberToReturn += numberToReturn + (numberList[i] + numberList[i+1])
+        for i in range(len(numberList)):
+            if i + 1 < len(numberList) and numberList[i] < numberList[i+1]:
+                numberToReturn -= numberList[i]
             else:
-                numberToReturn += numberToReturn + abs(numberList[i] - numberList[i+1])
+                numberToReturn += numberList[i]
         return numberToReturn
 
 
@@ -39,5 +39,17 @@ def createNumberList(romanList):
     return numberList
 
 
-print(romanToInteger(str))
+def romanToIntegerNeetcode(s):
+    roman = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+    res = 0
+    for i in range(len(s)):
+        if i+1 < len(s)  and roman[s[i]] < roman[s[i+1]]:
+            res -= roman[s[i]]
+        else:
+            res += roman[s[i]]
+    return res
 
+
+
+# print(romanToInteger(s))
+print(romanToIntegerNeetcode(s))
