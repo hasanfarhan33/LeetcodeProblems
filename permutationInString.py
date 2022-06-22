b8901c20a73a3df9096143203e79daaff91b6ef5
Farhan Hasan <hasanfarhan33@gmail.com>
2022-06-22 16:46:06 +0530

    permutationInString

--- permutationInString.py
+++ permutationInString.py
@@ -32,6 +32,39 @@ def checkInclusion(s1:str, s2:str) -> bool:
 
     print(s1Hash)
 
-def checkInclusionNeetcode()
+def checkInclusionNeetcode(s1: str, s2:str) -> bool:
+    if len(s1) > len(s2): return False
+    s1Count, s2Count = [0] * 26, [0] * 26
 
-checkInclusion(s1, s2)
+    for i in range(len(s1)):
+        s1Count[ord(s1[i]) - ord('a')] += 1
+        s2Count[ord(s2[i]) - ord('a')] += 1
+
+    matches = 0
+    for i in range(26):
+        matches += (1 if s1Count[i] == s2Count[i] else 0 )
+
+    left = 0
+    for right in range(len(s1), len(s2)):
+        if matches == 26: return True
+
+        # Adding the rightmost letter in the array
+        index = ord(s2[right]) - ord('a')
+        s2Count[index] += 1
+        if s2Count[index] == s1Count[index]:
+            matches += 1
+        elif s1Count[index] + 1 == s2Count[index]:
+            matches -= 1
+
+        # Removing the leftmost letter in the array
+        index = ord(s2[left]) - ord('a')
+        s2Count[index] -=1
+        if s2Count[index] == s1Count[index]:
+            matches += 1
+        elif s1Count[index] - 1 == s2Count[index]:
+            matches -= 1
+        left +=1
+    return matches == 26
+
+# checkInclusion(s1, s2)
+print(checkInclusionNeetcode(s1, s2))
